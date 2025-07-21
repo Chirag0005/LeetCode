61630119df40e6eb0e96fe02788277d3d7aa042f
Chirag <143305907+Chirag0005@users.noreply.github.com>
2025-07-21 15:09:07 +0530

    Time: 0 ms (100%), Space: 7.7 MB (87.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1 || n==2){
-            return n;
-        }
-        int a=1;
-        int b=2;
-        int c;
-        for(int i=3;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return b;
+     if(n==1){
+        return 1;
+     }
+     int dp[n+1];
+     dp[1]=1;
+     dp[2]=2;
+     for(int i=3;i<=n;i++){
+        dp[i]= dp[i-1]+dp[i-2];
+     }
+     return dp[n];   
     }
 };
