9921de7cf8f5fa0e8a4ef3e62e8bb6cd027b5f8f
Chirag <143305907+Chirag0005@users.noreply.github.com>
2025-07-24 09:58:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (62.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -32,10 +32,12 @@ public:
             while(list1){
                 tail->next = list1;
                 list1 = list1->next;
+                break;
             }
             while(list2){
                 tail->next = list2;
                 list2 = list2->next;
+                break;
             }
         
         return dummy->next;
