35d4b6b1849ecd3b90958920966adb4bfd32999a
Chirag <143305907+Chirag0005@users.noreply.github.com>
2025-08-21 13:40:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,7 +4,7 @@ public:
         int rev=0;
         while(x!=0){
             int digit = x%10;
-            if(rev > INT_MAX/10 || rev < INT_MIN/10){
+            if(rev*10 > INT_MAX || rev*10 < INT_MIN){
                 return 0;
             }
             rev = rev*10+ digit;
