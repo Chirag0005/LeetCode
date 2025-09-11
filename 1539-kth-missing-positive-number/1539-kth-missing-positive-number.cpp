8edacc6e570ec76b11f46954da5845d3c3b4b69e
Chirag <143305907+Chirag0005@users.noreply.github.com>
2025-09-11 14:24:32 +0530

    Time: 0 ms (100%), Space: 13.4 MB (9.25%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         int num=1;
+//         int index=0;
+//         while(true){
+//             if(index<arr.size() && arr[index]==num){
+//                 index++;
+//             }
+//             else {
+//                 k--;
+                
+//                 if(k==0) return num;
+//             }
+//             num++;
+//         }
+//     }
+// };
+
+//optimised
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int num=1;
-        int index=0;
-        while(true){
-            if(index<arr.size() && arr[index]==num){
-                index++;
+        int start =0; int end = arr.size()-1;int mid ; 
+        int ans = arr.size();//intiltised just beacause of the case when the array is already sorted
+
+        while(start<=end){
+            mid = start+(end-start)/2;
+            if(arr[mid]-mid -1 >= k){
+                end=mid-1;
+                ans=mid;
+            }else{
+                start = mid+1;
             }
-            else {
-                k--;
-                
-                if(k==0) return num;
-            }
-            num++;
         }
+        return ans+k;
     }
 };
