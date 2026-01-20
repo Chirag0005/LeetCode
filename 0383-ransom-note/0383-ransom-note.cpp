828252c648e0d2f8255234fe608f85d0f7282b81
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-01-20 19:10:11 +0530

    Time: 2 ms (62.16%), Space: 11.8 MB (72.48%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string s, string t) {
+        int arr[26]={0};
+        for(int i=0;i<t.length();i++){
+            char ch = t[i];
+            int number = ch-'a';
+            arr[number]++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            char ch = s[i];
+            int number = ch-'a';
+            arr[number]--;
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]<0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
