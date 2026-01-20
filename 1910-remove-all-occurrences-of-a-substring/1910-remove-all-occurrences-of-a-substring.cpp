3ad77e1862efe3dfd6ae27bf327c3dc4913b9ac3
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-01-21 01:16:11 +0530

    Time: 0 ms (100%), Space: 12 MB (10.77%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-//     string removeOccurrences(string s, string part) {
-//         string ans = "";
-//         int m = part.length();
-//         for(char c : s){
-//             ans.push_back(c);
-//             if(ans.length() > m && ans.substr(ans.size()-m,m)==part){
-//                 ans.erase(ans.size()-m,m);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-string removeOccurrences(string s, string part) {
-    string ans = "";
-    int m = part.size();
-
-    for(char c : s) {
-        ans.push_back(c);
-
-        if(ans.size() >= m && ans.substr(ans.size() - m, m) == part) {
-            ans.erase(ans.size() - m, m);
+    string removeOccurrences(string s, string part) {
+        string ans = "";
+        int m = part.length();
+        for(char c : s){
+            ans.push_back(c);
+            if(ans.length() >= m && ans.substr(ans.size()-m,m)==part){
+                ans.erase(ans.size()-m,m);
+            }
         }
+        return ans;
     }
-
-    return ans;
-}
 };
