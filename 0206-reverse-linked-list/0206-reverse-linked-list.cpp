30219025f11f6a7692af0db18a1377a2d24dbee9
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-02-02 11:36:32 +0530

    Time: 0 ms (100%), Space: 13.3 MB (91.67%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,27 +1,49 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//        ListNode* prev = nullptr;
+//        ListNode* curr = head;
+//        ListNode* next = nullptr;
+
+//        while(curr != nullptr){
+//         next = curr->next;
+//         curr->next = prev;
+
+//         prev = curr;
+//         curr= next;
+//        }
+//        return prev;
+//     }
+// };
+
+
 class Solution {
 public:
+    void reverse(ListNode* &head, ListNode* curr, ListNode* prev) {
+        // base case
+        if (curr == NULL) {
+            head = prev;
+            return;
+        }
+
+        ListNode* forward = curr->next; // store next
+        curr->next = prev;              // reverse link
+        reverse(head, forward, curr);
+    }
+
     ListNode* reverseList(ListNode* head) {
-       ListNode* prev = nullptr;
-       ListNode* curr = head;
-       ListNode* next = nullptr;
-
-       while(curr != nullptr){
-        next = curr->next;
-        curr->next = prev;
-
-        prev = curr;
-        curr= next;
-       }
-       return prev;
+        reverse(head, head, NULL);
+        return head;
     }
 };
+
