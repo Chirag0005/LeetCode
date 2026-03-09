8991012c5614563aa9210a9d7057b88ed02e8d23
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-03-09 15:20:09 +0530

    Time: 745 ms (10.09%), Space: 783.6 MB (7.57%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string str="";
-
-        for(char ch : s){
-            if(str.empty()|| str.back()!=ch){
-                str.push_back(ch);
+        stack<char> st;
+        for(char ch: s){
+            if(st.empty() || st.top()!=ch){
+                st.push(ch);
             }
             else{
-                str.pop_back();
+                st.pop();
             }
         }
-        return str;
+        
+        string ans="";
+
+        while(!st.empty()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+
+        return ans;
     }
 };
