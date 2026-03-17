bc91c7612cff829c8530a975faceb4b030af177d
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-03-18 01:18:37 +0530

    Time: 2 ms (8.5%), Space: 8.6 MB (33.42%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int add=0;
+        for(char ch : s){
+            if(ch=='('){
+                st.push(ch);
+            }
+            else{
+                if(st.size()>0){
+                    st.pop();
+                }
+                else{
+                    add++;
+                }
+            }
+        }
+        return add + st.size();
+    }
+};
