1250b0b53739bac2bfa09113a74c7330d266cffb
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-04-08 01:44:26 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonIndexZero=0;
+        // int nonIndexZero=0;
 
-        for(int i=0; i<nums.size();i++){
-            if(nums[i]!=0){
-                nums[nonIndexZero++]= nums[i];
+        // for(int i=0; i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //         nums[nonIndexZero++]= nums[i];
+        //     }
+        // }
+
+        // while(nonIndexZero < nums.size()){
+        //     nums[nonIndexZero++]= 0;
+        // }
+
+
+        int left = 0;
+        int right =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[right]!=0){
+                swap(nums[left],nums[right]);
+                left++;
+                right++;
             }
-        }
-
-        while(nonIndexZero < nums.size()){
-            nums[nonIndexZero++]= 0;
+            else{
+                right++;
+            }
         }
     }
 };
