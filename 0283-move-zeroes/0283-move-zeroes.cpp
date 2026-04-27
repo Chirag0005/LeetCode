4eae190fd500c1c39d49092e33fe3c7a663d84b7
Chirag <143305907+Chirag0005@users.noreply.github.com>
2026-04-27 05:47:31 +0530

    Time: 0 ms (100%), Space: 23.9 MB (54.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -8,12 +8,9 @@ public:
         //         nums[nonIndexZero++]= nums[i];
         //     }
         // }
-
         // while(nonIndexZero < nums.size()){
         //     nums[nonIndexZero++]= 0;
         // }
-
-
         int left = 0;
         int right =0;
         for(int i=0;i<nums.size();i++){
